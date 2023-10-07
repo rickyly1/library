b4081703fe9e63c225827803447257d075a65fed
Ricky Ly <lyricky123@gmail.com>
2023-10-06 20:12:04 -0700

    added open and close for add book menu

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
+const showPopupButton = document.getElementById("showPopup");
+const closePopupButton = document.getElementById("closePopup");
+const popupForm = document.getElementById("popup");
 const myLibrary = [];
 
+// Book object constructor
 function Book(title, author, pages, read, index) {
     this.title = title
     this.author = author
@@ -8,6 +12,17 @@ function Book(title, author, pages, read, index) {
     this.index = index
 }
 
+// iterates and displays myLibrary on DOM
 function addBookToLibrary() {
 
 }
+
+// button#showPopup displays book input form
+showPopupButton.addEventListener("click", () => {
+    popupForm.style.display = "block";
+})
+
+// #closePopup closes book input form
+closePopupButton.addEventListener("click", () => {
+    popupForm.style.display = "none";
+})
