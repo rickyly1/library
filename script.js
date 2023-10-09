57c82cabf5bc788ef869a058a11bd0568e42212a
Ricky Ly <lyricky123@gmail.com>
2023-10-08 19:01:28 -0700

    retrieves information from form and pushes array

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const showPopupButton = document.getElementById("showPopup");
 const closePopupButton = document.getElementById("closePopup");
 const popupForm = document.getElementById("popup");
+const addBookButton = document.getElementById("add-book");
 const myLibrary = [];
 
 // Book object constructor
@@ -14,7 +15,7 @@ function Book(title, author, pages, read, index) {
 
 // iterates and displays myLibrary on DOM
 function addBookToLibrary() {
-
+    
 }
 
 // button#showPopup displays book input form
@@ -24,5 +25,24 @@ showPopupButton.addEventListener("click", () => {
 
 // #closePopup closes book input form
 closePopupButton.addEventListener("click", () => {
+    popupForm.style.display = "none";
+})
+
+// makes a Book object and pushes into array
+addBookButton.addEventListener("click", () => {
+    const title = document.getElementById("title").value;
+    const author = document.getElementById("author").value;
+    const pages = parseInt(document.getElementById("pages").value);
+    const read = document.getElementById("read").checked;
+
+    // makes and pushes object to array
+    const newBook = new Book(title, author, pages, read, myLibrary.length + 1);
+    myLibrary.push(newBook);
+
+    // resets input fields
+    document.getElementById("title").value = "";
+    document.getElementById("author").value = "";
+    document.getElementById("pages").value = "";
+    document.getElementById("read").checked = false;
     popupForm.style.display = "none";
 })
